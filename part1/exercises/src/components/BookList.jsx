f1ae31642968319736845c6b2d2ca47d7d273390
Delaney Green <delaney.rosa1@gmail.com>
2024-02-22 15:07:26 -0600

    react exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,13 +1,13 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I want to read";
+   let book1 = "https://miblart.com/wp-content/uploads/2020/12/new-cover-mibl-2.jpeg";
+   let book2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQezFuKYrdptVPcuhK7-eaWjIz7-TQ1bFdz3v3dhtnRL4zYwP3OgBkGvvz_ap7Up3KyekM&usqp=CAU";
+   let book3 = "https://www.grapheine.com/wp-content/uploads/2017/04/1840-11.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
+         <img src={book1} alt="a book" />
          <img src={book2} alt="Nothing here!" />
          <img src={book3} alt="Nothing here!" />
       </div>      
